15e54b60075bf29f2901e3f7b5400e23f72def77
Muthia Rihadatul Aisyi <muthiarihadatul@gmail.com>
2025-12-31 09:43:55 +0700

    sixth commit

--- app/components/dashboard/DashboardPage.tsx
+++ app/components/dashboard/DashboardPage.tsx
@@ -1,14 +1,12 @@
-'use client';
-
-import SidebarWithLogo from './SidebarWithLogo';
-import StatsPanel from './StatsPanel';
-import PublicationSection from './PublicationSection';
+import { Suspense } from "react";
+import SidebarWithLogo from "./SidebarWithLogo";
+import StatsPanel from "./StatsPanel";
+import PublicationSection from "./PublicationSection";
 
 export default function DashboardPage() {
   return (
     <div className="flex min-h-screen flex-col">
-      
-      {/* MAIN CONTENT */}
+
       <main className="flex-1 px-4 md:px-8 py-6">
         <section
           className="
@@ -23,8 +21,10 @@ export default function DashboardPage() {
             <SidebarWithLogo />
           </aside>
 
-          {/* CENTER */}
-          <PublicationSection />
+          {/* CENTER (CLIENT) */}
+          <Suspense fallback={<div>Loading publications...</div>}>
+            <PublicationSection />
+          </Suspense>
 
           {/* STATS - DESKTOP */}
           <aside className="hidden lg:block">
--- app/page.tsx
+++ app/page.tsx
@@ -78,6 +78,7 @@ export default function HomePage() {
               transition
             "
           >
+            
             <i className="fas fa-search" />
             Search
           </button>
